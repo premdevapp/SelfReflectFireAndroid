504b96b51c62b5ca10d835c3264138835ddfccd1
premnath <premdevapp@gmail.com>
2020-12-05 05:13:15 +0530

    intent activity to post activity+++

--- app/src/main/java/com/example/myselfreflect/util/JournalApi.java
+++ app/src/main/java/com/example/myselfreflect/util/JournalApi.java
@@ -1,4 +1,36 @@
 package com.example.myselfreflect.util;
 
 public class JournalApi {
+
+    private String username;
+    private String userId;
+    private static JournalApi instance;
+
+    public static JournalApi getInstance() {
+        if (instance == null)
+            instance = new JournalApi();
+        return instance;
+
+    }
+
+    public JournalApi(){}
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+
 }
