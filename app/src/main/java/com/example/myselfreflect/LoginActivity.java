9d12f26cba3c95b8196fd0025989060b95b77493
premnath <premdevapp@gmail.com>
2020-12-04 17:23:32 +0530

    intent activity to post activity

--- app/src/main/java/com/example/myselfreflect/LoginActivity.java
+++ app/src/main/java/com/example/myselfreflect/LoginActivity.java
@@ -2,13 +2,59 @@ package com.example.myselfreflect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn, createAccBtn;
+    //firebase
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    //firestore connection
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
+
+    //get the widgets
+    private EditText emailEditText, passwordEditText;
+    private ProgressBar progressBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.email_sign_in_button);
+        createAccBtn = findViewById(R.id.create_acct_button_login);
+
+        createAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, CreateAccountActivity.class));
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/myselfreflect/MainActivity.java
+++ app/src/main/java/com/example/myselfreflect/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.myselfreflect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button getStartedBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getStartedBtn = findViewById(R.id.startButton);
+
+        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // go to login activity
+
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+            }
+        });
+
     }
 }
