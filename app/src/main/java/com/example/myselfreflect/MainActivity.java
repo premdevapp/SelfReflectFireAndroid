833bad516c52c400d6e942b08f761f17ebf09613
premnath <premdevapp@gmail.com>
2020-12-05 11:25:07 +0530

    intent activity to post activity+++++++

--- app/src/main/java/com/example/myselfreflect/MainActivity.java
+++ app/src/main/java/com/example/myselfreflect/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.myselfreflect;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,26 +9,102 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.myselfreflect.util.JournalApi;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-    private Button getStartedBtn;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
+
+    private FirebaseFirestore db =  FirebaseFirestore.getInstance();
+    private CollectionReference collectionReference = db.collection("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button getStartedButton = findViewById(R.id.startButton);
 
-        getStartedBtn = findViewById(R.id.startButton);
+        Objects.requireNonNull(getSupportActionBar()).setElevation(0);
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+                if (currentUser != null) {
+                    currentUser = firebaseAuth.getCurrentUser();
+                    final String currentUserId = currentUser.getUid();
 
-        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+                    collectionReference
+                            .whereEqualTo("userId", currentUserId)
+                            .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots,
+                                                    @Nullable FirebaseFirestoreException e) {
+                                    if (e != null) {
+                                        return;
+                                    }
+
+                                    String name;
+
+                                    if (!queryDocumentSnapshots.isEmpty()) {
+                                        for (QueryDocumentSnapshot snapshot : queryDocumentSnapshots) {
+                                            JournalApi journalApi = JournalApi.getInstance();
+                                            journalApi.setUserId(snapshot.getString("userId"));
+                                            journalApi.setUsername(snapshot.getString("username"));
+
+                                            startActivity(new Intent(MainActivity.this,
+                                                    JournalListActivity.class));
+                                            finish();
+
+
+                                        }
+                                    }
+
+                                }
+                            });
+
+                }else {
+
+                }
+            }
+        };
+
+
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // go to login activity
-
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-
+                //we go to LoginActivity
+                startActivity(new Intent(MainActivity.this,
+                        LoginActivity.class));
+                finish();
             }
         });
+    }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = firebaseAuth.getCurrentUser();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (firebaseAuth != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
     }
 }
